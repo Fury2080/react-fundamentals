69260bb4bf837c1021a123fa52d1f958ca6dce5b
Vaibhav Srivastava <fury65156@gmail.com>
2024-03-03 02:21:14 +0530

    eye killer gone

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import "./App.css";
-
 function App() {
-  return (
-    <div>
-      <h1>React basics</h1>
-      <p>Back again shit world</p>
+  return ( 
+    <div className= "min-h-screen bg-black text-white">
+      <h1 className="">React basics</h1>
+      <p className="">Back again shit world</p>
       <Button />
     </div>
   );
