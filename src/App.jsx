b42778610776e4280b7bbc3d2c574a99be037a4a
Vaibhav Srivastava <fury65156@gmail.com>
2024-03-03 02:40:52 +0530

    login form created, button done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 function App() {
-  return ( 
-    <div className= "min-h-screen bg-black text-white">
-      <h1 className="">React basics</h1>
-      <p className="">Back again shit world</p>
+  return (
+    <div className="min-h-screen bg-black text-white p-8 ">
+      <h1 className="text-8xl text-center">React basics</h1>
+      <p className="block">Back again shit world</p>
+      <LoginForm />
+    </div>
+  );
+}
+
+function LoginForm() {
+  return (
+    <div className="flex flex-col gap-4 w-fit mt-8 items-center">
+      <input type="text" />
+      <input type="password" />
       <Button />
     </div>
   );
 }
 
 function Button() {
-  return <button className="">kick me</button>;
+  return <button className="bg-blue-500 w-fit px-6 py-2 rounded-xl ">Login</button>;
 }
+
 export default App;
