448ad5fd0b1bb461546bac6b80b5911680ad73f8
Vaibhav Srivastava <fury65156@gmail.com>
2024-03-04 01:38:06 +0530

    Sign in colour given, lg changed to md (responsiveness)

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
+{
+  import("tailwindcss").Config;
+}
+
 function App() {
   return (
-    <div className="min-h-screen bg-black text-white p-4 lg:p-8 flex  flex-col justify-center items-center">
-      <h1 className="text-5xl lg:text-8xl text-center">React basics</h1>
+    <div className="min-h-screen bg-black text-white p-4 md:p-8 flex  flex-col justify-center items-center">
+      <h1 className="text-5xl md:text-8xl text-center">React basics</h1>
       <p className="block">Back again shit world</p>
       <LoginForm />
       <p>
-        Didn't have a account
+        Didn't have a account?
         <a
+          className="text-blue-500 visited:text-purple-500 underline"
           href="https://imagination-of-my-brain.vercel.app/"
           title="rederected to a new page"
           target="_blank"
         >
-        ? Sign in
+          Sign in
         </a>
       </p>
     </div>
@@ -21,12 +26,12 @@ function App() {
 function LoginForm() {
   return (
     <div className="flex flex-col gap-4 w-fit mt-8 items-center">
-      <div className="space-x-4 flex flex-col justify-between w-full lg:flex-row lg:items-center">
-        <label className="px-3 text-xl lg:p-0 lg:text-2xl">Username </label>
+      <div className="space-x-4 flex flex-col justify-between w-full md:flex-row md:items-center">
+        <label className="px-3 text-xl md:p-0 md:text-2xl">Username </label>
         <input className="text-black manrope px-3 py-1 " type="text" />
       </div>
-      <div className="space-x-4 flex flex-col justify-between w-full lg:flex-row lg:items-center">
-        <label className="text-xl px-3 lg:p-0 lg:text-2xl">Password </label>
+      <div className="space-x-4 flex flex-col justify-between w-full md:flex-row md:items-center">
+        <label className="text-xl px-3 md:p-0 md:text-2xl">Password </label>
         <input className="text-black manrope px-3 py-1" type="password" />
       </div>
       <Button />
